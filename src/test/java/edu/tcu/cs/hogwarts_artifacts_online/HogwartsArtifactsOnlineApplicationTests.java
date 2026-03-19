c71e00b02f47ee22074c5a1fe0513b01e2534f53
videogames21 <gtwhittingham@gmail.com>
2026-03-19 18:16:23 -0500

    This is the starting commit

---
+++ src/test/java/edu/tcu/cs/hogwarts_artifacts_online/HogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwarts_artifacts_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
